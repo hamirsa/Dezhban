253ca672f7a3781b788016493dfa0aceecc81753
Amir <hamirsa99@gmail.com>
2022-05-31 08:10:53 -0400

    CustomUser data model added.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    password = None
+    username = None 
+    phone_number = models.CharField(max_length=11, unique=True)
+        
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'<phone number: {self.phone_number}>'
